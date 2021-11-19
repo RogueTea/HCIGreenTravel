a173a857e95515f72af5e2f4f9268b6a5be26242
ChengPeng <60704371+chengpeng23588834@users.noreply.github.com>
2021-11-20 02:59:12 +0800

    Made log in page

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -22,28 +22,114 @@ void main() {
   ));
 }
 
+class TextBoxInput extends StatelessWidget {
+  final String _title;
+  //final Object _controller;
+  TextBoxInput(this._title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.fromLTRB(15.0, 0.0, 10.0, 0.0),
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        decoration: BoxDecoration(
+                      color: Color(0xffE3E3E3),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+        child: TextField(
+            autofocus: true,
+            obscureText: false,
+            style: TextStyle(
+              color: Color(0xff424242),
+              fontWeight: FontWeight.bold,
+              fontSize: 14,
+              letterSpacing: 1.2,
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: _title,
+              hintStyle: TextStyle(
+                  color: Color(0xff424242),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                  letterSpacing: 1.2),
+              
+            )));
+  }
+}
+
+
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffDDDFDE),
-      body: Center(
-            child: Column(
+      body: SingleChildScrollView(
+        child:Stack(
+        children: [
+          Positioned(
+                    top: 150,
+                    left: 80,
+                    height:60,
+                    width: 120,
+                    child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Color(0xff7DA4A8),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  child:Text("Log In",
+                    style: TextStyle(
+                      fontSize: 20, 
+                      fontWeight: FontWeight.bold,)))
+                ),
+          Positioned(
+                    top: 150,
+                    right: 80,
+                    height:60,
+                    width: 120,
+                    child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Color(0xffDDDFDE),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  child:Text("Register",
+                    style: TextStyle(
+                      fontSize: 20, 
+                      fontWeight: FontWeight.bold,)))
+                ),
+            Container(
+              margin: EdgeInsets.fromLTRB(50, 200, 50, 200),
+              padding: EdgeInsets.fromLTRB(10.0, 30, 10, 30),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: Color(0xffCBCBCB),
+                  borderRadius: BorderRadius.circular(15),
+              ),
+              child: Column(
           children: <Widget>[
-            ElevatedButton(
-              child: Text("Register"),
-              onPressed: () {
-                Navigator.pushNamed(context, '/register');
-              },
-            ),
-            ElevatedButton(
-              child: Text("Home"),
+            TextBoxInput("username"),
+            TextBoxInput("password"),
+           ElevatedButton(
+              child: Text("Log In", style: TextStyle(
+                fontSize: 16, 
+                color: Color(0xff232122),
+                fontWeight: FontWeight.bold)),
+              style: ElevatedButton.styleFrom(
+                padding:EdgeInsets.fromLTRB(30, 10, 30, 10),
+                primary: Color(0xff7DA4A8),
+                shadowColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),)),
               onPressed: () {
                 Navigator.pushNamed(context, '/home');
               },
-            )
+            ),
           ],
-        )));
+        )
+            )],
+            )));
   }
 }
 
