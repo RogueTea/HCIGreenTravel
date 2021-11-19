45e49fd2a131782b625ba75fd60a3ac4b1d4f053
Leeon Lau <2392383L@student.gla.ac.uk>
2021-11-19 15:52:08 +0000

    Blank pages with routing applied

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,115 +1,240 @@
 import 'package:flutter/material.dart';
 
+// Setup all the blank pages outlined in the figma wireframes.
+// Added basic button routing to map out the navigation of the app.
+
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    initialRoute: '/login',
+    routes: {
+      '/login': (context) => LoginPage(),
+      '/register': (context) => RegisterPage(),
+      '/home': (context) => HomePage(),
+      '/new-journey': (context) => NewJourneyPage(),
+      '/new-journey/confirm': (context) => NewJourneyConfirmPage(),
+      '/new-journey/save': (context) => NewJourneySavePage(),
+      '/new-journey/score': (context) => NewJourneyScoredPage(),
+      '/edit-journey': (context) => EditJourneyPage(),
+      '/scoreboard': (context) => ScoreboardPage(),
+      '/weekly-report': (context) => WeeklyReportPage(),
+      '/learn': (context) => LearnPage()
+    },
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        appBar: AppBar(
+          title: Text("Login"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+            child: Column(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            ElevatedButton(
+              child: Text("Register"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/register');
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ElevatedButton(
+              child: Text("Home"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+              },
+            )
+          ],
+        )));
+  }
+}
+
+class RegisterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text("Register"),
+      backgroundColor: Colors.blue,
+    ));
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Home"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+            child: Column(
+          children: <Widget>[
+            ElevatedButton(
+              child: Text("New Journey"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/new-journey');
+              },
+            ),
+            ElevatedButton(
+              child: Text("Edit Journey"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/edit-journey');
+              },
+            ),
+            ElevatedButton(
+              child: Text("Scoreboard"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/scoreboard');
+              },
+            ),
+            ElevatedButton(
+              child: Text("Weekly Report"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/weekly-report');
+              },
+            ),
+            ElevatedButton(
+              child: Text("Learn"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/learn');
+              },
+            )
+          ],
+        )));
+  }
+}
+
+class NewJourneyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Add New Journey"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+            child: Column(
+          children: <Widget>[
+            ElevatedButton(
+              child: Text("Save Journey"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/new-journey/save');
+              },
             ),
           ],
+        )));
+  }
+}
+
+class NewJourneyConfirmPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Save Journey"),
+          backgroundColor: Colors.blue,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+            child: Column(
+          children: <Widget>[
+            ElevatedButton(
+              child: Text("Save Journey"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/new-journey/confirm');
+              },
+            ),
+          ],
+        )));
+  }
+}
+
+class NewJourneySavePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Name Journey"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+            child: Column(
+          children: <Widget>[
+            ElevatedButton(
+              child: Text("Journey Score"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/new-journey/score');
+              },
+            ),
+          ],
+        )));
+  }
+}
+
+class NewJourneyScoredPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Journey Scored"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+            child: Column(
+          children: <Widget>[
+            ElevatedButton(
+              child: Text("Home"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+              },
+            ),
+          ],
+        )));
+  }
+}
+
+class EditJourneyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text("Edit Journey"),
+      backgroundColor: Colors.blue,
+    ));
+  }
+}
+
+class ScoreboardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text("Scoreboard"),
+      backgroundColor: Colors.blue,
+    ));
+  }
+}
+
+class WeeklyReportPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text("Weekly Report"),
+      backgroundColor: Colors.blue,
+    ));
+  }
+}
+
+class LearnPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text("Learn"),
+      backgroundColor: Colors.blue,
+    ));
   }
 }
