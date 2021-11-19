ba67dc5ec00eed3359f0b437dfeb498ecad71852
2386278f <2386278f@student.gla.ac.uk>
2021-11-19 16:08:52 +0000

    GET request not working

--- green_travel/api/models.py
+++ green_travel/api/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 import uuid
 
 # Create your models here.
 
-class Defaults(models.Model):
+""" class Defaults(models.Model):
     transport = models.CharField(max_length=10, primary_key=True)
     emissions = models.IntegerField(default =0)
 
     def __str__(self):
-        return self.transport
+        return self.transport """
 
 class Journey(models.Model):
     date = models.DateField((u"Conversation Date"), blank=True)
@@ -17,7 +18,7 @@ class Journey(models.Model):
     journeyid =  models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     distance = models.IntegerField(default =0)
     emitted = models.IntegerField(default =0)
-    transport = models.ForeignKey(Defaults,n_delete=models.CASCADE, default = "Car")
+    #transport = models.ForeignKey(Defaults,n_delete=models.CASCADE, default = "Car")
 
     def __str__(self):
         return self.journeyid
@@ -29,7 +30,7 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-class Scoreboard(models.Model):
+""" class Scoreboard(models.Model):
     journeyid = models.ForeignKey(Journey, on_delete=models.CASCADE)
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-
+ """
