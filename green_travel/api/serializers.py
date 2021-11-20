011d2415f0302d7284007268817c023801615cc1
Kalok Chan <2388188c@student.gla.ac.uk>
2021-11-20 23:28:20 +0000

    attempt at choice field for learn page

--- green_travel/api/serializers.py
+++ green_travel/api/serializers.py
@@ -1,15 +1,26 @@
+from rest_framework.fields import ChoiceField
 from rest_framework.serializers import ModelSerializer
 from .models import *
 
 class DefaultSerializer(ModelSerializer):
+
+    def calculate(self, validated_data):
+        data = validated_data
+        transport = ChoiceField(choices=Default.objects.all())
+        emissions = Default.objects.get(pk=transport)
+        distance = data['distance']
+        emitted = transport.emissions * distance
+
     class Meta:
         model = Default
         fields = '__all__'
 
 class JourneySerializer(ModelSerializer):
+
     def create(self,validated_data):
         data = validated_data
         transport = Default.objects.get(pk=data['transport'])
+        #emissions = Default.objects.get(pk=transport)
         distance = data['distance']
 
         journey = Journey.objects.create(
--- green_travel/api/views.py
+++ green_travel/api/views.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers, status
-from api.forms import JourneyForm
 from api.models import *
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -7,10 +6,18 @@ from api.serializers import *
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def co2Learn(request):
-    learn = Default.objects.all()
-    serializer = DefaultSerializer (learn, many=True)
+    if request.method == "GET":
+            learn = Default.objects.all()
+            serializer = DefaultSerializer (learn, many=True)
+    elif request.method == "POST":
+            serializer = DefaultSerializer (data=request.data)
+            if serializer.is_valid(raise_exception = ValueError):
+                serializer.calculate(validated_data=request.data)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response({"error": True,"error_msg": serializer.error_messages},status=status.HTTP_400_BAD_REQUEST)
     return Response(serializer.data)
 
 @api_view(['POST'])
